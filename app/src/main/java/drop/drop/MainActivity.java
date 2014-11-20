3a9cd04b770d0289c0be0cc3b289bdab53adcfa3
Kyle Dillon <kjdillon3@gmail.com>
2014-11-19 21:27:16 -0800

    Basic Firebase communication works.

--- app/src/main/java/drop/drop/MainActivity.java
+++ app/src/main/java/drop/drop/MainActivity.java
@@ -6,8 +6,14 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 public class MainActivity extends Activity {
+
+    Firebase dropFirebase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +21,18 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         ActionBar actionBar = getActionBar();
         actionBar.hide();
-    }
 
+        Firebase.setAndroidContext(this);
+        // I made the database under my account, ask me and I'll add your account as a developer on it. -Kyle
+        dropFirebase = new Firebase("https://dropdatabase.firebaseio.com/");
+        dropFirebase.child("drops").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                System.out.println(snapshot.getValue()); // Simple reading data test.
+            }
+            @Override public void onCancelled(FirebaseError error) { }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
